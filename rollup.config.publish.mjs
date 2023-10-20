4f5574dddc86aaaeb3aef0a17690e531a6e1c3a1
kmar <minedreams@qq.com>
2023-10-20 18:15:31 +0800

    压缩 HTML

--- rollup.config.publish.mjs
+++ rollup.config.publish.mjs
@@ -14,7 +14,20 @@ export default {
     plugins: [
         resolve(),
         html({
-            include: './src/resources/**/*.html'
+            include: './src/resources/**/*.html',
+            htmlMinifierOptions: {
+                collapseWhitespace: true,               // 合并空格
+                collapseBooleanAttributes: true,        // 压缩布尔类型的 attributes
+                noNewlinesBeforeTagClose: false,        // 去掉换行符
+                removeAttributeQuotes: true,            // 在可能时删除属性值的引号
+                removeRedundantAttributes: true,        // 属性值与默认值一样时删除属性
+                removeEmptyAttributes: true,            // 删除值为空的属性
+                removeScriptTypeAttributes: true,       // 删除 `type="text/javascript"`
+                removeStyleLinkTypeAttributes: true,    // 删除 `type="text/css"`
+                minifyJS: true,                         //压缩页面 JS
+                minifyCSS: true,                        //压缩页面 CSS
+                minifyURLs: true                        //压缩页面URL
+            }
         }),
         postcss({
             inject: false,
