e150477564dc7c08a9858189a8394259f03d4031
kmar <minedreams@qq.com>
2023-11-04 13:17:04 +0800

    修改压缩参数

--- rollup.config.publish.mjs
+++ rollup.config.publish.mjs
@@ -1,15 +1,29 @@
 import terser from '@rollup/plugin-terser'
+import replace from '@rollup/plugin-replace'
 
 import config from './rollup.config.common.mjs'
 
+// noinspection SpellCheckingInspection
 config.plugins.push(
     terser({
         compress: {
-            sequences: 50,
+            sequences: true,
             unsafe: true,
+            unsafe_arrows: true,
             unsafe_math: true,
             pure_getters: true,
+            booleans_as_integers: true,
+            drop_console: true,
+            passes: 2,
             ecma: 2015
+        }
+    }),
+    replace({
+        preventAssignment: true,
+        values: {
+            'const ': 'let ',
+            '!==': '!=',
+            '===': '=='
         }
     })
 )
