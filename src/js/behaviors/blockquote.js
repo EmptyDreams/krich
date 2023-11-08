c034e2ed0492284353d06e993920f0b017f1c024
kmar <minedreams@qq.com>
2023-11-08 19:50:48 +0800

    添加/删除引用时保留原有样式

--- src/js/behaviors/blockquote.js
+++ src/js/behaviors/blockquote.js
@@ -5,8 +5,11 @@ import * as RangeUtils from '../range'
  * 引用按钮的点击事件
  */
 export function behaviorBlockquote() {
-    const isBlockquote = container =>
-        container && [container.nodeName, container.parentNode.nodeName].includes('BLOCKQUOTE')
+    const isBlockquote = container => {
+        if (!container) return false
+        const parent = container.parentNode
+        return [container, parent, parent.parentNode].find(it => it.nodeName === 'BLOCKQUOTE')
+    }
     /**
      * 查找最近的一行的起点
      * @param blockquote {HTMLQuoteElement}
@@ -14,7 +17,7 @@ export function behaviorBlockquote() {
      * @return {number}
      */
     const findLineStartIndex = (blockquote, startOffset) =>
-        blockquote.textContent.lastIndexOf('\n', startOffset) + 1
+        blockquote.innerHTML.lastIndexOf('\n', startOffset) + 1
     /**
      * 查找最近的一行的终点
      * @param blockquote {HTMLQuoteElement}
@@ -22,12 +25,12 @@ export function behaviorBlockquote() {
      * @return {number}
      */
     const findLineEndIndex = (blockquote, endOffset) =>
-        blockquote.textContent.indexOf('\n', endOffset)
-    const buildBlockquote = content => {
+        blockquote.innerHTML.indexOf('\n', endOffset)
+    const buildBlockquote = html => {
         const blockquote = document.createElement('blockquote')
         blockquote.setAttribute('data-id', 'blockquote')
         blockquote.setAttribute('data-stamp', Date.now().toString(16))
-        blockquote.textContent = content
+        blockquote.innerHTML = html
         return blockquote
     }
     const range = getSelection().getRangeAt(0)
@@ -41,15 +44,15 @@ export function behaviorBlockquote() {
     if (isBlockquote(startContainer) && startContainer.parentNode === endContainer.parentNode) {
         /* 如果选择范围在一个引用中，则取消选择的区域的引用 */
         const blockquote = startContainer.parentElement
-        const content = blockquote.textContent
-        const selectedContents = (start, end) => content
+        const html = blockquote.innerHTML
+        const selectedHtml = (start, end) => html
             .substring(start, end)
             .split('\n')
             .map(it => `<p>${it}</p>`)
             .join('')
         /** 清除整个引用 */
         const removeAll = () => {
-            blockquote.insertAdjacentHTML('afterend', selectedContents(0))
+            blockquote.insertAdjacentHTML('afterend', selectedHtml(0))
             blockquote.remove()
         }
         // 如果是只点击了一下则判定为选择了全部
@@ -59,15 +62,15 @@ export function behaviorBlockquote() {
         if (start === 0 && end === -1) {    // 如果包含了整个引用
             removeAll()
         } else if (start === 0) {   // 如果包含第一行
-            blockquote.insertAdjacentHTML('beforebegin', selectedContents(0, end))
-            blockquote.textContent = content.substring(end + 1)
+            blockquote.insertAdjacentHTML('beforebegin', selectedHtml(0, end))
+            blockquote.innerHTML = html.substring(end + 1)
         } else if (end === -1) {    // 如果包含了最后一行
-            blockquote.insertAdjacentHTML('afterend', selectedContents(start))
-            blockquote.textContent = content.substring(0, start)
+            blockquote.insertAdjacentHTML('afterend', selectedHtml(start))
+            blockquote.innerHTML = html.substring(0, start)
         } else {    // 如果是夹在中间
-            const ps = selectedContents(start, end)
-            const bottomBlockquote = buildBlockquote(content.substring(end + 1))
-            blockquote.textContent = content.substring(0, start)
+            const ps = selectedHtml(start, end)
+            const bottomBlockquote = buildBlockquote(html.substring(end + 1))
+            blockquote.innerHTML = html.substring(0, start)
             blockquote.insertAdjacentElement('afterend', bottomBlockquote)
             blockquote.insertAdjacentHTML('afterend', ps)
         }
@@ -85,31 +88,28 @@ export function behaviorBlockquote() {
     } else if (!existing) {
         existing = lines.find(it => it.nodeName === 'BLOCKQUOTE')
     }
-    let newContent = lines.map(it => {
-        const content = it.textContent
-        return content.endsWith('\n') ? content.substring(0, content.length - 1) : content
-    }).join('\n')
-    if (newContent.length === 0) newContent = '\n'
+    let newHtml = lines.map(it => it.innerHTML).join('\n')
+    if (newHtml.length === 0) newHtml = '\n'
     if (existing) { // 如果已经存在一个引用，则将所有内容合并到这个引用中
         switch (mode) {
             case 0:
-                existing.textContent = newContent
+                existing.innerHTML = newHtml
                 break
             case 1: // 0b01
-                existing.textContent = newContent + '\n' + existing.textContent
+                existing.innerHTML = newHtml + '\n' + existing.innerHTML
                 break
             case 2: // 0b10
-                existing.textContent += '\n' + newContent
+                existing.innerHTML += '\n' + newHtml
                 break
             case 3: // 0b11
-                lines[0].previousSibling.textContent += '\n' + newContent + '\n' + existing.textContent
+                lines[0].previousSibling.innerHtml += '\n' + newHtml + '\n' + existing.innerHTML
                 existing.remove()
                 break
         }
     } else {    // 否则新建一个引用
-        const blockquote = buildBlockquote(newContent)
+        const blockquote = buildBlockquote(newHtml)
         lines[0].parentNode.insertBefore(blockquote, lines[0])
-        setCursorPosition(blockquote.firstChild, isEnd ? newContent.length : startOffset)
+        setCursorPosition(blockquote.firstChild, isEnd ? blockquote.textContent.length : startOffset)
     }
     // 移除原有的标签
     lines.filter(it => it !== existing)
