edcffabd03b806d6cd47288b375165fe231381d5
kmar <minedreams@qq.com>
2023-11-10 19:47:47 +0800

    添加自定义的 beforeinput 事件

---
+++ src/js/events/before-input.js
@@ -0,0 +1,15 @@
+/**
+ * 注册 before input 事件。
+ *
+ * 该事件会在用户输入内容前触发，中文输入过程中不会触发该事件。
+ *
+ * @param target {HTMLElement}
+ * @param consumer {function(Event)}
+ */
+export function registryBeforeInputEventListener(target, consumer) {
+    target.addEventListener('beforeinput', event => {
+        if (event.isComposing) event.preventDefault()
+        else consumer(event)
+    })
+    target.addEventListener('compositionend', consumer)
+}
