138130c8c33d099d36fcc28c21758bd4a861d7e7
kmar <minedreams@qq.com>
2023-11-26 21:18:26 +0800

    设置标题后自动修正鼠标光标位置

--- src/js/behaviors/header.js
+++ src/js/behaviors/header.js
@@ -1,4 +1,5 @@
 import {SELECT_VALUE} from '../global-fileds'
+import {KRange} from '../range'
 
 /**
  * 标题按钮的点击事件
@@ -8,10 +9,12 @@ import {SELECT_VALUE} from '../global-fileds'
 export function behaviorHeader(range, target) {
     const value = target.getAttribute(SELECT_VALUE)
     console.assert(value?.length === 1, `${value} 值异常`)
+    const data = range.serialization()
     range.getAllTopElements().forEach(item => {
         const novel = document.createElement(value === '0' ? 'p' : 'h' + value)
         novel.setAttribute('data-id', 'headerSelect')
         novel.innerHTML = item.textContent
         item.replaceWith(novel)
     })
+    KRange.deserialized(data).active()
 }
