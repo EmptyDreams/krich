f20c93887bf59f187c1e5007bda118ad4bc9fbe2
kmar <minedreams@qq.com>
2024-01-25 22:01:43 +0800

    不再允许多个分割线相邻

--- src/js/behaviors/hr.js
+++ src/js/behaviors/hr.js
@@ -10,13 +10,17 @@ import {setCursorPositionBefore} from '../utils/range'
 export function onclickHr(range) {
     const posLine = findParentTag(range.endContainer, TOP_LIST)
     const hr = createElement('hr')
-    if (!posLine.nextSibling) {
-        posLine.insertAdjacentElement('afterend', createNewLine())
-    }
     if (posLine.innerHTML === '<br>') {
         posLine.replaceWith(hr)
     } else {
         posLine.insertAdjacentElement('afterend', hr)
+    }
+    const {previousSibling, nextSibling} = hr
+    if (!previousSibling || previousSibling.nodeName === 'HR') {
+        hr.insertAdjacentElement('beforebegin', createNewLine())
+    }
+    if (!nextSibling || nextSibling.nodeName === 'HR') {
+        hr.insertAdjacentElement('afterend', createNewLine())
     }
     setCursorPositionBefore(hr.nextSibling)
 }
