c6e652d87edb20dce568000d0e70c33f3fb148d2
kmar <minedreams@qq.com>
2024-02-10 17:01:10 +0800

    如果在代码高亮期间移动了光标，则高亮后不再重置光标

--- src/js/utils/highlight.js
+++ src/js/utils/highlight.js
@@ -1,5 +1,6 @@
 import {highlight} from '../vars/global-fileds'
 import {KRange} from './range'
+import {editorRange} from '../events/range-monitor'
 
 /**
  * 高亮代码
@@ -10,10 +11,12 @@ import {KRange} from './range'
 export async function highlightCode(range, pre) {
     console.assert(pre?.nodeName === 'PRE', '传入的 pre 必须是代码块对象', pre)
     if (!highlight) return false
+    const old = editorRange
     const isArray = Array.isArray(range)
     const offlineData = isArray ? range : range.serialization()
     const cb = highlight(pre)
     if (cb) await cb
-    KRange.deserialized(offlineData).active()
+    if (old === editorRange)
+        KRange.deserialized(offlineData).active()
     return true
 }
