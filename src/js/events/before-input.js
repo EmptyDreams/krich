32998836743e730b6af2ba25729d50e88376876c
kmar <minedreams@qq.com>
2024-02-20 18:16:44 +0800

    内联 `before-input` 代码

--- src/js/events/before-input.js
+++ src/js/events/before-input.js
@@ -1,31 +1,67 @@
 // noinspection JSAssignmentUsedAsCondition
 
 import {editorRange, updateEditorRange} from './range-monitor'
-import {markComposingStart, markComposingStop} from '../vars/global-fileds'
+import {
+    KRICH_EDITOR,
+    markComposingStart,
+    markComposingStop,
+    markStatusCacheEffect,
+    statusCheckCache
+} from '../vars/global-fileds'
 import {findParentTag} from '../utils/dom'
 import {highlightCode} from '../utils/highlight'
+import {KRange} from '../utils/range'
+import {compareBtnListStatusWith} from '../utils/btn'
+import {getElementBehavior} from '../utils/tools'
+import {TODO_MARKER} from '../vars/global-tag'
 
 let codeHighlight
 
 /**
- * 注册 before input 事件。
- *
- * 该事件会在用户输入内容前触发，中文输入过程中不会触发该事件。
- *
- * @param target {HTMLElement}
- * @param consumer {function(InputEvent|CompositionEvent):Promise<void>}
+ * 注册 before input 事件
  */
-export function registryBeforeInputEventListener(target, consumer) {
-    target.addEventListener('beforeinput', event => {
+export function registryBeforeInputEventListener() {
+    const handler = async event => setTimeout(() => {
+        const {data, inputType} = event
+        let range = KRange.activated()
+        const {startContainer, startOffset} = range
+        if (findParentTag(range.realStartContainer(), ['PRE'])) return
+        /* 当用户输入位置所在文本与按钮列表不同时，将新输入的文本样式与按钮状态同步 */
+        if (data && !statusCheckCache && range.collapsed) {
+            markStatusCacheEffect()
+            const buttonList = compareBtnListStatusWith(startContainer)
+            if (!buttonList) return
+            const newRange = new KRange()
+            newRange.setStart(startContainer, startOffset - data.length)
+            newRange.setEnd(startContainer, startOffset)
+            const offline = newRange.serialization()
+            for (let child of buttonList) {
+                const behavior = getElementBehavior(child)
+                behavior.onclick(KRange.deserialized(offline), child)
+            }
+            newRange.deserialized(offline)
+            newRange.collapse(false)
+            newRange.active()
+        }
+        /* 在代办列表中换行时自动在 li 中插入 <input> */
+        if (inputType === 'insertParagraph') {
+            const todoList = findParentTag(startContainer, item => item.classList?.contains?.('todo'))
+            if (todoList) {
+                const item = todoList.querySelector('&>li>p:first-child')
+                if (item) item.insertAdjacentElement('beforebegin', TODO_MARKER.cloneNode(true))
+            }
+        }
+    }, 0)
+    KRICH_EDITOR.addEventListener('beforeinput', event => {
         if (event.isComposing) {
             markComposingStart()
         } else if (!highlightCodeHelper() && event.inputType.startsWith('insert')) {
             // noinspection JSIgnoredPromiseFromCall
-            consumer(event)
+            handler(event)
         }
     })
-    target.addEventListener('compositionend', event => {
-        consumer(event).then(() => {
+    KRICH_EDITOR.addEventListener('compositionend', event => {
+        handler(event).then(() => {
             markComposingStop()
             updateEditorRange()
         })
--- src/js/utils/init.js
+++ src/js/utils/init.js
@@ -37,40 +37,7 @@ export function initKrich(optional) {
     registryRangeMonitor()
     registryEditorScrollEvent()
     registryIntersectionObserverEvent()
-    registryBeforeInputEventListener(KRICH_EDITOR, event => new Promise(resolve => {
-        setTimeout(() => {
-            resolve()
-            const {data, inputType} = event
-            let range = KRange.activated()
-            const {startContainer, startOffset} = range
-            if (findParentTag(range.realStartContainer(), ['PRE'])) return
-            /* 当用户输入位置所在文本与按钮列表不同时，将新输入的文本样式与按钮状态同步 */
-            if (data && !statusCheckCache && range.collapsed) {
-                markStatusCacheEffect()
-                const buttonList = compareBtnListStatusWith(startContainer)
-                if (!buttonList) return
-                const newRange = new KRange()
-                newRange.setStart(startContainer, startOffset - data.length)
-                newRange.setEnd(startContainer, startOffset)
-                const offline = newRange.serialization()
-                for (let child of buttonList) {
-                    const behavior = getElementBehavior(child)
-                    behavior.onclick(KRange.deserialized(offline), child)
-                }
-                newRange.deserialized(offline)
-                newRange.collapse(false)
-                newRange.active()
-            }
-            /* 在代办列表中换行时自动在 li 中插入 <input> */
-            if (inputType === 'insertParagraph') {
-                const todoList = findParentTag(startContainer, item => item.classList?.contains?.('todo'))
-                if (todoList) {
-                    const item = todoList.querySelector('&>li>p:first-child')
-                    if (item) item.insertAdjacentElement('beforebegin', TODO_MARKER.cloneNode(true))
-                }
-            }
-        }, 0)
-    }))
+    registryBeforeInputEventListener()
 }
 
 /**
