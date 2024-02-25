f1b0c67dace759a62d803d357723c7517b52b25b
kmar <minedreams@qq.com>
2024-02-25 11:19:11 +0800

    修复创建代码块时有时高亮滞后的问题

--- src/js/behaviors/highlight.js
+++ src/js/behaviors/highlight.js
@@ -1,7 +1,7 @@
 import {behaviors} from '../vars/global-fileds'
-import {findParentTag} from '../utils/dom'
+import {findParentTag, getFirstChildNode} from '../utils/dom'
 import {isEmptyLine} from '../utils/tools'
-import {KRange} from '../utils/range'
+import {KRange, setCursorPositionBefore} from '../utils/range'
 
 /**
  * 代码高亮按钮点击
@@ -18,7 +18,7 @@ export function behaviorHighlight(range) {
         } else {
             line.insertAdjacentElement('afterend', pre)
         }
-        range.active(true)
+        setCursorPositionBefore(getFirstChildNode(pre))
     } else {
         const offlineData = range.serialization()
         const allLines = range.getAllTopElements()
