a1651409f50d5dd53418524f0f11a14931e33e71
kmar <minedreams@qq.com>
2024-04-23 17:04:21 +0800

    添加代码块的操作支持撤回

--- src/js/behaviors/highlight.js
+++ src/js/behaviors/highlight.js
@@ -2,26 +2,29 @@ import {findParentTag, getFirstChildNode} from '../utils/dom'
 import {isCommonLine, isEmptyLine} from '../utils/tools'
 import {KRange, setCursorPositionBefore} from '../utils/range'
 import {behaviors} from '../behavior'
+import {GLOBAL_HISTORY} from '../vars/global-fileds'
 
 /**
  * 代码高亮按钮点击
  * @param range {KRange}
  */
 export function behaviorHighlight(range) {
+    const offlineData = GLOBAL_HISTORY.initRange(range, true)
     const behavior = behaviors.code
     const pre = behavior.builder()
     const code = pre.firstChild
     if (range.collapsed) {
         const line = findParentTag(range.startContainer, isCommonLine)
         if (isEmptyLine(line)) {
-            line.replaceWith(pre)
+            GLOBAL_HISTORY.utils.replace(line, [pre])
         } else {
             line.after(pre)
+            GLOBAL_HISTORY.addAfter(line, [pre])
         }
         setCursorPositionBefore(getFirstChildNode(pre))
     } else {
-        const offlineData = range.serialization()
         const allLines = range.getAllTopElements()
+        GLOBAL_HISTORY.removeAuto(allLines)
         allLines[0].after(pre)
         let codeContent = ''
         for (let line of allLines) {
@@ -29,6 +32,8 @@ export function behaviorHighlight(range) {
             line.remove()
         }
         code.textContent = codeContent
+        GLOBAL_HISTORY.addAuto([pre])
         range.deserialized(offlineData).active(true)
     }
+    GLOBAL_HISTORY.next()
 }
